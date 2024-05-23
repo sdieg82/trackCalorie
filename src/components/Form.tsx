6bbe340106cecc84d997767aec7ddbc840548280
Diego <sdieg82@gmail.com>
2024-05-23 14:49:28 -0500

    add reducer to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
-import { useState,ChangeEvent } from "react"
+import { useState,ChangeEvent,FormEvent, Dispatch } from "react"
 import { categories } from "../data/categories"
 import { Activity } from "../types"
+import { ActivityActions } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps={
+    dispatch:Dispatch<ActivityActions>
+}
+
+export default function Form({dispatch}:FormProps) {
 
    const [activity,setActivity]=useState<Activity>({
     category:1,
@@ -22,9 +27,17 @@ export default function Form() {
     const {name,calories}=activity
     return name.trim() !== '' && calories>0
    }
+   const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    dispatch({
+        type:'save-activity',
+        payload:{newActivity: activity}
+    })
+   }
   
    return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg"
+    onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoria:</label>
             <select className="border border-slate-300 p-2 rounded-lg w-full bg-white"  
